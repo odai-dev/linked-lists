c2d5c7a84f1f46a501eca6cc3d4ba83a02fb0db1
Odai Gubran <95129948+odai-dev@users.noreply.github.com>
2025-12-04 15:30:13 +0300

    add prepend method to prepend a node to the beigining of the list

--- LinkedList.js
+++ LinkedList.js
@@ -1,13 +1,16 @@
 import { Node } from "./Node.js";
 class LinkedList {
     _head = null;
-    _tail = null
+    _tail = null;
+
     head() {
         return this._head;
     }
+
     tail() {
         return this._tail;
     }
+
     append(value) {
         if(!this._head) {
             const node = new Node(value);
@@ -24,10 +27,23 @@ class LinkedList {
         current.nextNode = node;
         this._tail = node;
     }
+
+    prepend(value) {
+        if(!this._head) {
+            const node = new Node(value);
+            this._head = node;
+            this._tail = node;
+            return;
+        }
+        const node = new Node(value);
+        node.nextNode = this._head;
+        this._head = node;
+    }
 }
 
 const list = new LinkedList();
 console.log(list.head(), list.tail());
 list.append("cat");
+list.prepend("cow");
 list.append("dog");
 console.log("Head: ",list.head(), "Tail: ",list.tail());
